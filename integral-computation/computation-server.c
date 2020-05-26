864c1a73e68180dfaafe902a73d6b406c18d03d2
Vladik <valeinik00@gmail.com>
2020-05-26 11:24:41 +0300

    Fixed Client Keepalive

--- integral-computation/computation-server.c
+++ integral-computation/computation-server.c
@@ -7,7 +7,7 @@
 
 const size_t num_tasks   = 8;
 const double start_point = 1.0;
-const double end_point   = 51.0;
+const double end_point   = 101.0;
 const double diff        = 0.00000001;
 
 struct task_data
